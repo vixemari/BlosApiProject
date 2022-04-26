75fd9aa1cce2d8197c192f371d7c4cccbf8d95aa
Mariana Rocha Silva <marianarocha308@gmail.com>
2022-04-26 18:39:22 -0300

    adiciona o migration de users

---
+++ migrations/20220426212757-Users.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users',{
+      id:{
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      displayName:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
