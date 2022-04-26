72f47be78dbee4f57f89343b37821b1e93f3375a
Mariana Rocha Silva <marianarocha308@gmail.com>
2022-04-26 18:51:47 -0300

    adiciona o migration de postCategories

---
+++ migrations/20220426214515-postCategories.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories',{
+      postId:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'BlogPosts',
+          keys: 'id',
+        },
+      },
+      categoryId:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'Categories',
+          keys: 'id',
+      },
+      },
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');    
+  }
+};
